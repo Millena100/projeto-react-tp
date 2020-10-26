8b51ef1df6f379d4258b203c666224f979ff7349
Millena <milsaca7@gmail.com>
2020-10-26 17:00:36 -0300

    react-projeto

---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,71 @@
+import React from 'react';
+
+import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component';
+import {auth, signInWithGoogle} from '../../firebase/firebase.utils.js';
+import './sign-in.styles.scss';
+
+class SignIn extends React.Component{
+    constructor(props){
+        super(props);
+
+        this.state ={
+            email: '',
+            password: ''
+        }
+    }
+
+    handleSubmit = async event => {
+        event.preventDefault();
+
+        const {email, password} = this.state;
+
+        try{
+          await auth.signInWithEmailAndPassword(email, password);
+          this.setState({ email: "", password: "" });
+        }catch(error){
+          console.log(error)
+        }
+    }
+
+    handleChange = event => {
+        const { value, name } = event.target;
+
+        this.setState({ [name]: value })
+    }
+
+    render(){
+        return (
+          <div className="sign-in">
+            <h2>Já tenho uma conta</h2>
+            <span>Entre com o seu email e sua senha</span>
+
+            <form onSubmit={this.handleSubmit}>
+              <FormInput
+                name="email"
+                type="email"
+                handleChange={this.handleChange}
+                value={this.state.email}
+                label="Email"
+                required
+              />
+
+              <FormInput
+                name="password"
+                type="password"
+                value={this.state.password}
+                handleChange={this.handleChange}
+                label="Senha"
+                required
+              />
+              <div className='buttons' >
+                <CustomButton type="submit"> Entrar </CustomButton>
+                <CustomButton onClick={signInWithGoogle} isGoogleSignIn > Usar conta Google </CustomButton>
+              </div>
+            </form>
+          </div>
+        );
+    }
+}
+
+export default SignIn;
---
+++ src/redux/shop/shop.data.js
@@ -0,0 +1,67 @@
+const SHOP_DATA = [
+  {
+    id: 1,
+    title: "Produtos",
+    routeName: "",
+    items: [
+      {
+        id: 1,
+        name: "",
+        imageUrl: "https://i.ibb.co/ZYW3VTp/brown-brim.png",
+        price: 25
+      },
+      {
+        id: 2,
+        name: "",
+        imageUrl: "https://i.ibb.co/ypkgK0X/blue-beanie.png",
+        price: 18
+      },
+      {
+        id: 3,
+        name: "",
+        imageUrl: "https://i.ibb.co/QdJwgmp/brown-cowboy.png",
+        price: 35
+      },
+      {
+        id: 4,
+        name: "",
+        imageUrl: "https://i.ibb.co/RjBLWxB/grey-brim.png",
+        price: 25
+      },
+    ]
+  },
+  {
+    id: 2,
+    title: "",
+    routeName: "",
+    items: [
+      {
+        id: 5,
+        name: "",
+        imageUrl: "https://i.ibb.co/YTjW3vF/green-beanie.png",
+        price: 25
+      },
+      {
+        id: 6,
+        name: "",
+        imageUrl: "https://i.ibb.co/rKBDvJX/palm-tree-cap.png",
+        price: 18
+      },
+      {
+        id: 7,
+        name: "",
+        imageUrl: "https://i.ibb.co/bLB646Z/red-beanie.png",
+        price: 35
+      },
+      {
+        id: 8,
+        name: "",
+        imageUrl: "https://i.ibb.co/1f2nWMM/wolf-cap.png",
+        price: 25
+      },
+    ]
+  },
+];
+
+export default SHOP_DATA;
+
